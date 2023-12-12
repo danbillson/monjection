db74adea7de861e01fd7f64c1ae41aceaca5c1fc
Dan Billson <dbillson@outlook.com>
2023-12-12 17:39:11 +0000

    🐛 fix: add logger to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Monster } from "@/components/monster";
 import { FetchClient } from "@/lib/http";
+import { ConsoleLogger } from "@/lib/logger";
 import { PokemonService } from "@/lib/monster";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 import { container } from "tsyringe";
 
+container.register("Logger", { useClass: ConsoleLogger });
 container.register("HTTPClient", { useClass: FetchClient });
 
 const monsterService = container.resolve(PokemonService);
@@ -19,8 +21,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold text-center">{monster.name}</h1>
-      <Image width={96} height={96} src={monster.sprite} alt={monster.name} />
+      <Monster monster={monster} />
     </main>
   );
 }
